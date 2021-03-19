22c96eeb2aa6255cee52500e3d3e23663096b0d7
FilinGoy <den_suvorov2003@mail.ru>
2021-03-19 12:19:46 +0500

    JS

--- js/script.js
+++ js/script.js
@@ -1,137 +1,171 @@
-data = [
-    {   
-        numOfRooms: 1, 
-        address: "Салавата Юлаева, 17а", 
-        district: "Калининский район", 
-        square: 34, 
-        floor: 7, 
-        numOfFloors: 10, 
-        price: 9500
-    }, 
-    {   
-        numOfRooms: 2, 
-        address: "Свободы, 104а", 
-        district: "Советский район", 
-        square: 60, 
-        floor: 2, 
-        numOfFloors: 5, 
-        price: 9000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Ереванская, 14", 
-        district: "Ленинский район", 
-        square: 40, 
-        floor: 3, 
-        numOfFloors: 9, 
-        price: 9000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Дарвина, 107", 
-        district: "Советский район", 
-        square: 31, 
-        floor: 5, 
-        numOfFloors: 5, 
-        price: 9000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Ереванская, 14", 
-        district: "Ленинский район", 
-        square: 40, 
-        floor: 3, 
-        numOfFloors: 9, 
-        price: 9000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Молодогвардейцев, 21", 
-        district: "Курчатовский район", 
-        square: 40, 
-        floor: 8, 
-        numOfFloors: 10, 
-        price: 10000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Двинская, 3", 
-        district: "Калининский район", 
-        square: 49, 
-        floor: 10, 
-        numOfFloors: 10, 
-        price: 11500
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Комарова, 112", 
-        district: "Тракторозаводский район", 
-        square: 34, 
-        floor: 6, 
-        numOfFloors: 9, 
-        price: 8000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Котина, 21", 
-        district: "Тракторозаводский район", 
-        square: 34, 
-        floor: 4, 
-        numOfFloors: 10, 
-        price: 8000
-    },
-    {   
-        numOfRooms: 2, 
-        address: "Ленина проспект, 28", 
-        district: "Центральный район", 
-        square: 60, 
-        floor: 4, 
-        numOfFloors: 10, 
-        price: 17000
-    },
-    {   
-        numOfRooms: 3, 
-        address: "Петра Сумина, 2", 
-        district: "Центральный район", 
-        square: 84, 
-        floor: 15, 
-        numOfFloors: 19, 
-        price: 15000
-    },
-    {   
-        numOfRooms: 2, 
-        address: "Комарова, 114а ", 
-        district: "Тракторозаводский район", 
-        square: 50, 
-        floor: 4, 
-        numOfFloors: 10, 
-        price: 8000
-    },
-    {   
-        numOfRooms: 2, 
-        address: "Свободы, 92а", 
-        district: "Центральный район", 
-        square: 54, 
-        floor: 2, 
-        numOfFloors: 5, 
-        price: 12000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Академика Сахарова, 22 ", 
-        district: "Центральный район", 
-        square: 43, 
-        floor: 7, 
-        numOfFloors: 10, 
-        price: 14000
-    },
-    {   
-        numOfRooms: 1, 
-        address: "Академика Сахарова, 30", 
-        district: "Центральный район", 
-        square: 43, 
-        floor: 3, 
-        numOfFloors: 10, 
-        price: 13000
-    }
-]
+data = [{   
+    id: 1,                              // id
+    numOfRooms: 1,                      // Кол-во комнат
+    address: "Салавата Юлаева, 17а",    // Адрес
+    district: "Калининский район",      // Район
+    square: 34,                         // Площадь n^2
+    floor: 7,                           // Этаж
+    numOfFloors: 10,                    // Всего этажей в доме
+    price: 9500,                        // Цена
+    photo: "content/gg"                 // Фото
+}, 
+{   
+    id: 2,
+    numOfRooms: 2, 
+    address: "Свободы, 104а", 
+    district: "Советский район", 
+    square: 60, 
+    floor: 2, 
+    numOfFloors: 5, 
+    price: 9000,
+    photo: "content/gg"
+},
+{   
+    id: 3,
+    numOfRooms: 1, 
+    address: "Ереванская, 14", 
+    district: "Ленинский район", 
+    square: 40, 
+    floor: 3, 
+    numOfFloors: 9, 
+    price: 9000,
+    photo: "content/gg"
+},
+{   
+    id: 4,
+    numOfRooms: 1, 
+    address: "Дарвина, 107", 
+    district: "Советский район", 
+    square: 31, 
+    floor: 5, 
+    numOfFloors: 5, 
+    price: 9000,
+    photo: "content/gg"
+},
+{   
+    id: 5,
+    numOfRooms: 1, 
+    address: "Ереванская, 14", 
+    district: "Ленинский район", 
+    square: 40, 
+    floor: 3, 
+    numOfFloors: 9, 
+    price: 9000,
+    photo: "content/gg"
+},
+{   
+    id: 6,
+    numOfRooms: 1, 
+    address: "Молодогвардейцев, 21", 
+    district: "Курчатовский район", 
+    square: 40, 
+    floor: 8, 
+    numOfFloors: 10, 
+    price: 10000,
+    photo: "content/gg"
+},
+{   
+    id: 7,
+    numOfRooms: 1, 
+    address: "Двинская, 3", 
+    district: "Калининский район", 
+    square: 49, 
+    floor: 10, 
+    numOfFloors: 10, 
+    price: 11500,
+    photo: "content/gg"
+},
+{   
+    id: 8,
+    numOfRooms: 1, 
+    address: "Комарова, 112", 
+    district: "Тракторозаводский район", 
+    square: 34, 
+    floor: 6, 
+    numOfFloors: 9, 
+    price: 8000,
+    photo: "content/gg"
+},
+{   
+    id: 9,
+    numOfRooms: 1, 
+    address: "Котина, 21", 
+    district: "Тракторозаводский район", 
+    square: 34, 
+    floor: 4, 
+    numOfFloors: 10, 
+    price: 8000,
+    photo: "content/gg"
+},
+{   
+    id: 10,
+    numOfRooms: 2, 
+    address: "Ленина проспект, 28", 
+    district: "Центральный район", 
+    square: 60, 
+    floor: 4, 
+    numOfFloors: 10, 
+    price: 17000,
+    photo: "content/gg"
+},
+{   
+    id: 11,
+    numOfRooms: 3, 
+    address: "Петра Сумина, 2", 
+    district: "Центральный район", 
+    square: 84, 
+    floor: 15, 
+    numOfFloors: 19, 
+    price: 15000,
+    photo: "content/gg"
+},
+{   
+    id: 12,
+    numOfRooms: 2, 
+    address: "Комарова, 114а ", 
+    district: "Тракторозаводский район", 
+    square: 50, 
+    floor: 4, 
+    numOfFloors: 10, 
+    price: 8000,
+    photo: "content/gg"
+},
+{   
+    id: 13,
+    numOfRooms: 2, 
+    address: "Свободы, 92а", 
+    district: "Центральный район", 
+    square: 54, 
+    floor: 2, 
+    numOfFloors: 5, 
+    price: 12000,
+    photo: "content/gg"
+},
+{   
+    id: 14,
+    numOfRooms: 1, 
+    address: "Академика Сахарова, 22 ", 
+    district: "Центральный район", 
+    square: 43, 
+    floor: 7, 
+    numOfFloors: 10, 
+    price: 14000,
+    photo: "content/gg"
+},
+{   
+    id: 15,
+    numOfRooms: 1, 
+    address: "Академика Сахарова, 30", 
+    district: "Центральный район", 
+    square: 43, 
+    floor: 3, 
+    numOfFloors: 10, 
+    price: 13000,
+    photo: "content/gg"
+}]
+
+var d = document;
+var vst = d.getElementById("main-img");
+for (var i=0; i<data.lenght; i++) {
+    innerHTML+=``
+}
